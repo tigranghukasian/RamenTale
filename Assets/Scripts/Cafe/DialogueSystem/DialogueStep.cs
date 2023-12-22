d2b10557543a9fae03c3eab832c00a39c9c00f15
moskmosk <tigran.ghukasian@gmail.com>
2023-12-22 11:16:28 +0400

    Added ConfirmStepActionType and BeginStepActionType in DialogueStep

--- Assets/Scripts/Cafe/DialogueSystem/DialogueStep.cs
+++ Assets/Scripts/Cafe/DialogueSystem/DialogueStep.cs
@@ -11,11 +11,27 @@ public class DialogueStep
     [TextArea(3,10)]
     [SerializeField] private string stepText;
 
+    public enum BeginStepActionType
+    {
+        
+    }
+
+    public enum ConfirmStepActionType
+    {
+        
+    }
+
+    [SerializeField] private BeginStepActionType beginStepActionFunction;
+    [SerializeField] private ConfirmStepActionType confirmStepActionFunction;
+
     public string StepText { get => stepText; set => stepText = value; }
 
     public Action OnConfirmStepCustomAction { get; protected set; }
     
     public Action OnBeginStepAction { get; protected set; }
+
+    public BeginStepActionType BeginStepActionFunction => beginStepActionFunction;
+    public ConfirmStepActionType ConfirmStepActionFunction => confirmStepActionFunction;
     
     
     public void AddConfirmAction(Action action)
@@ -28,4 +44,5 @@ public class DialogueStep
     }
 
 }
+
 
